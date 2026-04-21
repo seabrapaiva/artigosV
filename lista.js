84b3798820698ae21c114e206f2e65a6ff1b2f5f
Vasco <45953077+seabrapaiva@users.noreply.github.com>
2026-04-21 21:11:59 +0200

    Update lista.js

--- lista.js
+++ lista.js
@@ -4,5 +4,7 @@ const itensGaleria = [
     { "arquivo": "IMG (53).jpg", "nome": "Microondas", "name": "Microwave", "disponivel": true },
     { "arquivo": "IMG (54).jpg", "nome": "Torradeira", "name": "Toaster", "disponivel": true },
     { "arquivo": "IMG (55).jpg", "nome": "Chaleira", "name": "kettle", "disponivel": true },
-    { "arquivo": "IMG (92).jpg", "nome": "Tapete Ginástica", "name": "Gym Mat", "disponivel": true }
+    { "arquivo": "IMG (92).jpg", "nome": "Tapete Ginástica", "name": "Gym Mat", "disponivel": true },
+    { "arquivo": "IMG (100).jpg", "nome": "TV Caixa caracteristicas", "name": "TV Specs", "disponivel": true },
+    { "arquivo": "IMG (101).jpg", "nome": "Smart TV", "name": "Smart TV", "disponivel": true }
 ];
