d69c51f00ac9fd8d41ac2103925789eb5ab5f18f
Vasco <45953077+seabrapaiva@users.noreply.github.com>
2026-04-21 20:36:19 +0200

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,8 @@
+const itensGaleria = [
+    { "arquivo": "IMG (99).jpg", "nome": "Mitsubichi Space Star", "name": "**2020**", "disponivel": true },
+    { "arquivo": "IMG (52).jpg", "nome": "Maq Expresso Philips", "name": "Expresso MAchine Philips", "disponivel": true },
+    { "arquivo": "IMG (53).jpg", "nome": "Microondas", "name": "Microwave", "disponivel": true },
+    { "arquivo": "IMG (54).jpg", "nome": "Torradeira", "name": "Toaster", "disponivel": true },
+    { "arquivo": "IMG (55).jpg", "nome": "Chaleira", "name": "kettle", "disponivel": true },
+    { "arquivo": "IMG (92).jpg", "nome": "Tapete Ginástica", "name": "Gym Mat", "disponivel": true }
+];
